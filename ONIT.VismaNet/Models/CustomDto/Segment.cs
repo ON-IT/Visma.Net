651374f6be9e17b1def1b73aae8d9c617e84c541
Sierd Weidema <sierd.weidema@arch-it.nl>
2017-01-02 13:57:23 +0100

    Set segmentId to 1 if not set. The first segment should be 1 since version 6.01

--- ONIT.VismaNet/Models/CustomDto/Segment.cs
+++ ONIT.VismaNet/Models/CustomDto/Segment.cs
@@ -2,7 +2,7 @@
 {
     public class Segment
     {
-        public int segmentId { get; set; }
+        public int segmentId { get; set; } = 1; // since version 6.01 this should by default 1, instead of 0
         public string segmentDescription { get; set; }
         public string segmentValue { get; set; }
         public string segmentValueDescription { get; set; }
