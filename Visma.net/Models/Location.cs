1db144e9301d641bd4e191b64f415c562fa65e7c
Benny Olsson <benny.olsson@infostep.se>
2017-12-16 21:33:36 +0100

    Added minimal support for /location

---
+++ Visma.net/Models/Location.cs
@@ -0,0 +1,77 @@
+﻿using ONIT.VismaNetApi.Lib;
+using ONIT.VismaNetApi.Models.CustomDto;
+
+namespace ONIT.VismaNetApi.Models
+{
+    public class Location : DtoProviderBase, IProvideIdentificator
+    {
+        public string baccountId
+        {
+            get { return Get<string>(); }
+            set { Set(value); }
+        }
+
+        public string locationId
+        {
+            get { return Get<string>(); }
+            set { Set(value); }
+        }
+
+        public string locationName
+        {
+            get { return Get<string>(); }
+            set { Set(value); }
+        }
+
+        public bool active
+        {
+            get { return Get<bool>(); }
+            set { Set(value); }
+        }
+
+        public bool addressIsSameAsMain
+        {
+            get { return Get<bool>(); }
+            set { Set(value); }
+        }
+
+        public Address address { get { return Get<Address>(); } set { Set(value); } }
+
+        public bool contactIsSameAsMain
+        {
+            get { return Get<bool>(); }
+            set { Set(value); }
+        }
+
+        public ContactInfo contact { get { return Get<ContactInfo>(); } set { Set(value); } }
+
+        public string vatRegistrationId
+        {
+            get { return Get<string>(); }
+            set { Set(value); }
+        }
+
+        public string vatZone
+        {
+            get { return Get<string>(); }
+            set { Set(value); }
+        }
+
+        public string ediCode
+        {
+            get { return Get<string>(); }
+            set { Set(value); }
+        }
+
+        public string gln
+        {
+            get { return Get<string>(); }
+            set { Set(value); }
+        }
+
+        public string GetIdentificator()
+        {
+            return baccountId;
+        }
+    }
+}
