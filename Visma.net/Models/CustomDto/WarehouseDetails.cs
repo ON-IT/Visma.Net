d20812c71e7f2104a70fecbe76ff2e0dd7659de1
Ole Melhus <ole@on-it.no>
2018-04-13 12:57:36 +0200

    Extended warehousedetails

--- Visma.net/Models/CustomDto/WarehouseDetails.cs
+++ Visma.net/Models/CustomDto/WarehouseDetails.cs
@@ -5,5 +5,7 @@
         public bool isDefault { get; set; }
         public string warehouse { get; set; }
         public decimal quantityOnHand { get; set; }
+        public decimal available { get; set; }
+        public decimal availableForShipment { get; set; }
     }
 }
