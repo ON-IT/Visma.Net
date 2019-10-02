6d14014198c73118e616cd6029ffe330abcfec72
Anders Yderborg <anders.yderborg@traventus.se>
2019-10-02 15:44:26 +0200

    Added defaultpaymentmethod on customer

---
+++ Visma.net/Models/CustomDto/CustomerPaymentMethod.cs
@@ -0,0 +1,21 @@
+﻿using ONIT.VismaNetApi.Lib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ONIT.VismaNetApi.Models.CustomDto
+{
+    public class CustomerPaymentMethod : DtoProviderBase
+    {
+        public string paymentMethodId
+        {
+            get => Get<string>();
+            set => Set(value);
+        }
+        public bool isDefault
+        {
+            get => Get<bool>();
+            set => Set(value);
+        }
+    }
+}
