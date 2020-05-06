611eb40f3b064e9064433ce0f0d0f87a842b6e9a
Mikael Romberg <mikael.romberg@iosoft.se>
2020-05-06 11:06:10 +0200

    Add CustomerSalesPrice endpoint with paging

---
+++ Visma.net/lib/Data/CustomerSalesPriceData.cs
@@ -0,0 +1,17 @@
+﻿using ONIT.VismaNetApi.Lib;
+using ONIT.VismaNetApi.Lib.Data;
+using ONIT.VismaNetApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ONIT.VismaNetApi.Lib.Data
+{
+    public class CustomerSalesPriceData : BasePaginatedCrudDataClass<CustomerSalesPrice>
+    {
+        public CustomerSalesPriceData(VismaNetAuthorization auth) : base(auth)
+        {
+            ApiControllerUri = VismaNetControllers.CustomerSalesPrices;
+        }
+    }
+}
