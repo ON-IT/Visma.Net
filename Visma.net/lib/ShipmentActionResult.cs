c14061cbed9945927cd648fb819679061f862c52
col-papaavola <56531418+col-papaavola@users.noreply.github.com>
2020-05-28 08:14:23 +0300

    Update Visma.net/lib/ShipmentActionResult.cs
    Co-authored-by: Ole Melhus <ole@on-it.no>

--- Visma.net/lib/ShipmentActionResult.cs
+++ Visma.net/lib/ShipmentActionResult.cs
@@ -6,7 +6,7 @@ namespace ONIT.VismaNetApi.Lib
     public class CreateShipmentActionResult : VismaActionResult
     {
         [JsonProperty("referenceNumber")]
-        public string ReferenceNumber { get; internal set; }
+        public string referenceNumber { get; internal set; }
 
         [JsonProperty("shipmentDto")]
         public Models.Shipment shipment { get; internal set; }
