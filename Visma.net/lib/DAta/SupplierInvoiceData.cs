e92648d986d2999d3ba573290be2648719fc6601
Ole Melhus <ole@on-it.no>
2021-01-12 12:18:58 +0100

    fix: allow uploading attachments to supplier invoices with different document types

--- Visma.net/lib/DAta/SupplierInvoiceData.cs
+++ Visma.net/lib/DAta/SupplierInvoiceData.cs
@@ -57,19 +57,24 @@ namespace ONIT.VismaNetApi.Lib.Data
             }
         }
 
-        public async Task<string> AddAttachmentToInvoice(string invoiceNumber, string content, string fileName)
+        public async Task<string> AddAttachmentToInvoice(string invoiceNumber, string content, string fileName, SupplierDocumentType documentType = SupplierDocumentType.Invoice)
         {
-                return await AddAttachmentToInvoice(invoiceNumber, Encoding.UTF8.GetBytes(content), fileName);
+                return await AddAttachmentToInvoice(invoiceNumber, Encoding.UTF8.GetBytes(content), fileName, documentType);
         }
-        public async Task<string> AddAttachmentToInvoice(string invoiceNumber, byte[] byteArray, string fileName)
+
+        public async Task<string> AddAttachmentToInvoice(string invoiceNumber, byte[] byteArray, string fileName, SupplierDocumentType documentType = SupplierDocumentType.Invoice)
         {
             if (byteArray == default(byte[]))
                 throw new ArgumentNullException(nameof(byteArray), "ByteArray is missing");
             if (string.IsNullOrEmpty(fileName) || string.IsNullOrEmpty(Path.GetExtension(fileName)))
                 throw new ArgumentNullException(nameof(fileName), "File name must be provided and have an extention");
+            if(documentType != SupplierDocumentType.Invoice)
+            {
+                invoiceNumber = $"documentType/{documentType}/{invoiceNumber}";
+            }
             return await VismaNetApiHelper.AddAttachmentToSupplierInvoice(Authorization, invoiceNumber, byteArray, fileName);
         }
-        public async Task<string> AddAttachmentToInvoice(string invoiceNumber, Stream stream, string fileName)
+        public async Task<string> AddAttachmentToInvoice(string invoiceNumber, Stream stream, string fileName, SupplierDocumentType documentType = SupplierDocumentType.Invoice)
         {
             if (stream == default(Stream))
                 throw new ArgumentNullException(nameof(stream), "Stream is missing");
@@ -79,6 +84,10 @@ namespace ONIT.VismaNetApi.Lib.Data
             using (var memoryStream = new MemoryStream())
             {
                 stream.CopyTo(memoryStream);
+                if (documentType != SupplierDocumentType.Invoice)
+                {
+                    invoiceNumber = $"documentType/{documentType}/{invoiceNumber}";
+                }
                 return await VismaNetApiHelper.AddAttachmentToSupplierInvoice(Authorization, invoiceNumber, memoryStream.ToArray(), fileName);
             }
 
