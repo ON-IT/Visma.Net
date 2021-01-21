86b58bc02f005b67e22e1d8f5c7adb5dd721dc47
Ole Melhus <ole@on-it.no>
2021-01-21 14:38:45 +0100

    fix: issue with empty response from general ledger transaction

--- Visma.net/lib/VismaNetApiHelper.cs
+++ Visma.net/lib/VismaNetApiHelper.cs
@@ -399,6 +399,8 @@ namespace ONIT.VismaNetApi.Lib
         {
             var firstPage = await GetAll<T>(ApiControllerUri, Authorization, CreatePagionationParameters(initialPageSize, 1, parameters));
             var rsp = new List<T>();
+            if (firstPage == null)
+                return rsp;
             rsp.AddRange(firstPage);
             var count = firstPage.Sum(x => x.GetSubCount());
             if (firstPage.FirstOrDefault()?.metadata?.totalCount > count && count > 0)
