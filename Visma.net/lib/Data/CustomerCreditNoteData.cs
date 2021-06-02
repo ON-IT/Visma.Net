fff8227e6fc4d89245344d92c055955d7ff051ef
reositter <reositter@outlook.com>
2021-06-02 10:33:05 +0200

    Update CustomerCreditNoteData.cs
    Redirect CustomerCreditNote Get to V1

--- Visma.net/lib/Data/CustomerCreditNoteData.cs
+++ Visma.net/lib/Data/CustomerCreditNoteData.cs
@@ -6,55 +6,56 @@ using System.Threading.Tasks;
 
 namespace ONIT.VismaNetApi.Lib.Data
 {
-    public class CustomerCreditNoteData : BasePaginatedCrudDataClass<CustomerCreditNote>
+  public class CustomerCreditNoteData : BasePaginatedCrudDataClass<CustomerCreditNote>
+  {
+    /// <summary>
+    ///     This constructor is called by the client to create the data source.
+    /// </summary>
+    internal CustomerCreditNoteData(VismaNetAuthorization auth) : base(auth)
     {
-        /// <summary>
-		///     This constructor is called by the client to create the data source.
-		/// </summary>
-		internal CustomerCreditNoteData(VismaNetAuthorization auth) : base(auth)
-        {
-            ApiControllerUri = VismaNetControllers.CustomerCreditNote;
-        }
-
-        protected CustomerCreditNoteData() : base(null)
-        {
-            ApiControllerUri = VismaNetControllers.CustomerCreditNote;
-        }
-
-        public override async Task<CustomerCreditNote> Add(CustomerCreditNote entity)
-        {
-            var rsp = await VismaNetApiHelper.Create(entity, VismaNetControllers.CustomerCreditNoteV2, Authorization);
-            rsp.InternalPrepareForUpdate();
-            return rsp;
-        }
-
-        public async Task<string> AddAttachmentToCreditNote(string creditNoteNumber, string content, string fileName)
-        {
-            return await AddAttachmentToCreditNote(creditNoteNumber, Encoding.UTF8.GetBytes(content), fileName);
-        }
-
-        public async Task<string> AddAttachmentToCreditNote(string creditNoteNumber, byte[] byteArray, string fileName)
-        {
-            if (byteArray == default(byte[]))
-                throw new ArgumentNullException(nameof(byteArray), "ByteArray is missing");
-            if (string.IsNullOrEmpty(fileName) || string.IsNullOrEmpty(Path.GetExtension(fileName)))
-                throw new ArgumentNullException(nameof(fileName), "File name must be provided and have an extention");
-            return await VismaNetApiHelper.AddAttachmentToCreditNote(Authorization, creditNoteNumber, byteArray, fileName);
-        }
-
-        public async Task<string> AddAttachmentToCreditNote(string creditNoteNumber, Stream stream, string fileName)
-        {
-            if (stream == default(Stream))
-                throw new ArgumentNullException(nameof(stream), "Stream is missing");
-            if (string.IsNullOrEmpty(fileName) || string.IsNullOrEmpty(Path.GetExtension(fileName)))
-                throw new ArgumentNullException(nameof(fileName), "File name must be provided and have an extention");
-
-            using (var memoryStream = new MemoryStream())
-            {
-                stream.CopyTo(memoryStream);
-                return await VismaNetApiHelper.AddAttachmentToCreditNote(Authorization, creditNoteNumber, stream, fileName);
-            }
-        }
+      ApiControllerUri = VismaNetControllers.CustomerCreditNote;
     }
+
+    protected CustomerCreditNoteData() : base(null)
+    {
+      ApiControllerUri = VismaNetControllers.CustomerCreditNote;
+    }
+
+    // Redirect to V2 since V1 Post will be depricated
+    public override async Task<CustomerCreditNote> Add(CustomerCreditNote entity)
+    {
+      var rsp = await VismaNetApiHelper.Create(entity, VismaNetControllers.CustomerCreditNoteV2, Authorization,VismaNetControllers.CustomerCreditNote);
+      rsp.InternalPrepareForUpdate();
+      return rsp;
+    }
+
+    public async Task<string> AddAttachmentToCreditNote(string creditNoteNumber, string content, string fileName)
+    {
+      return await AddAttachmentToCreditNote(creditNoteNumber, Encoding.UTF8.GetBytes(content), fileName);
+    }
+
+    public async Task<string> AddAttachmentToCreditNote(string creditNoteNumber, byte[] byteArray, string fileName)
+    {
+      if (byteArray == default(byte[]))
+        throw new ArgumentNullException(nameof(byteArray), "ByteArray is missing");
+      if (string.IsNullOrEmpty(fileName) || string.IsNullOrEmpty(Path.GetExtension(fileName)))
+        throw new ArgumentNullException(nameof(fileName), "File name must be provided and have an extention");
+      return await VismaNetApiHelper.AddAttachmentToCreditNote(Authorization, creditNoteNumber, byteArray, fileName);
+    }
+
+    public async Task<string> AddAttachmentToCreditNote(string creditNoteNumber, Stream stream, string fileName)
+    {
+      if (stream == default(Stream))
+        throw new ArgumentNullException(nameof(stream), "Stream is missing");
+      if (string.IsNullOrEmpty(fileName) || string.IsNullOrEmpty(Path.GetExtension(fileName)))
+        throw new ArgumentNullException(nameof(fileName), "File name must be provided and have an extention");
+
+      using (var memoryStream = new MemoryStream())
+      {
+        stream.CopyTo(memoryStream);
+        return await VismaNetApiHelper.AddAttachmentToCreditNote(Authorization, creditNoteNumber, stream, fileName);
+      }
+    }
+  }
 }
 
