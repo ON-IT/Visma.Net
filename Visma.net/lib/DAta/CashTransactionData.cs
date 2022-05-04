eeeaf2018af172f8d6f55b9830ac30385b40d643
Ole Melhus <ole@on-it.no>
2022-05-04 10:01:11 +0200

    remove: appveyor

--- Visma.net/lib/DAta/CashTransactionData.cs
+++ Visma.net/lib/DAta/CashTransactionData.cs
@@ -4,26 +4,26 @@ using System.Threading.Tasks;
 namespace ONIT.VismaNetApi.Lib.Data
 {
 
-  public class CashTransactionData : BasePaginatedCrudDataClass<CashTransaction>
-  {
-    internal CashTransactionData(VismaNetAuthorization auth) : base(auth)
+    public class CashTransactionData : BasePaginatedCrudDataClass<CashTransaction>
     {
-      ApiControllerUri = VismaNetControllers.CashTransaction;
-    }
+        internal CashTransactionData(VismaNetAuthorization auth) : base(auth)
+        {
+            ApiControllerUri = VismaNetControllers.CashTransaction;
+        }
 
-    protected CashTransactionData() : base(null)
-    {
-      ApiControllerUri = VismaNetControllers.CashTransaction;
-    }
+        protected CashTransactionData() : base(null)
+        {
+            ApiControllerUri = VismaNetControllers.CashTransaction;
+        }
 
-    public async Task<VismaActionResult> Release(CashTransaction payment)
-    {
-      return await VismaNetApiHelper.Action(Authorization, ApiControllerUri, payment.GetIdentificator(), "release", new { type = new DtoValue(payment.refNbr) });
-    }
+        public async Task<VismaActionResult> Release(CashTransaction payment)
+        {
+            return await VismaNetApiHelper.Action(Authorization, ApiControllerUri, payment.GetIdentificator(), "release", new { type = new DtoValue(payment.refNbr) });
+        }
 
-    public async Task<VismaActionResult> Void(CashTransaction payment)
-    {
-      return await VismaNetApiHelper.Action(Authorization, ApiControllerUri, payment.GetIdentificator(), "void", new { type = new DtoValue(payment.refNbr) });
+        public async Task<VismaActionResult> Void(CashTransaction payment)
+        {
+            return await VismaNetApiHelper.Action(Authorization, ApiControllerUri, payment.GetIdentificator(), "void", new { type = new DtoValue(payment.refNbr) });
+        }
     }
-  }
 }
