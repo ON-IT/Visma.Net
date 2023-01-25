fdb35d7441719752c1de4a1b0d3e2075280d9ca7
Sierd Weidema <Sierd.Weidema@arch-it.nl>
2023-01-25 15:24:37 +0100

    Get method implemented

--- Visma.net/lib/Data/ProjectTransactionData.cs
+++ Visma.net/lib/Data/ProjectTransactionData.cs
@@ -1,6 +1,7 @@
 ﻿using ONIT.VismaNetApi.Lib;
 using ONIT.VismaNetApi.Lib.Data;
 using ONIT.VismaNetApi.Models;
+using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
 using System.Threading.Tasks;
@@ -14,14 +15,7 @@ namespace ONIT.VismaNetApi.lib.Data
             ApiControllerUri = VismaNetControllers.ProjectTransaction;
         }
 
-        public override async Task<List<ProjectTransaction>> All() => await this.Find(new NameValueCollection());
-        public override async Task<List<ProjectTransaction>> Find(NameValueCollection parameters)
-        {
-            // parameter fromPeriod and toPeriod are mandatory
-            // Check if parameters are in the collection if not throw exception
-            /*            if (parameters.Keys.)
-
-                            return await VismaNetApiHelper.GetAllWithPagination<ProjectTransaction>(ApiControllerUri, Authorization, parameters);*/
-        }
+        public override async Task<List<ProjectTransaction>> All() => throw new NotImplementedException("This method is not implemented in the Visma.net Api");
+        public override async Task<List<ProjectTransaction>> Find(NameValueCollection parameters) => await VismaNetApiHelper.GetAllWithPagination<ProjectTransaction>(ApiControllerUri, Authorization, parameters);
     }
 }
