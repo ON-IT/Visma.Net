1af0f732b70f7feb09646706f75686f2b118074e
Anders Yderborg <anders.yderborg@traventus.se>
2023-10-26 16:28:17 +0200

    4.2.1.9

--- Visma.net/VismaNet.cs
+++ Visma.net/VismaNet.cs
@@ -238,7 +238,7 @@ namespace ONIT.VismaNetApi
         {
             return await VismaNetApiHelper.GetTokenOAuth(client_id, client_secret, code, redirect_uri);
         }
-        internal static async Task<VismaConnectToken> GetTokenFromVismaConnect(string clientId, string secret, string tenant_id, string scope = "vismanet_erp_service_api:create vismanet_erp_service_api:delete vismanet_erp_service_api:read vismanet_erp_service_api:update")
+        public static async Task<VismaConnectToken> GetTokenFromVismaConnect(string clientId, string secret, string tenant_id, string scope = "vismanet_erp_service_api:create vismanet_erp_service_api:delete vismanet_erp_service_api:read vismanet_erp_service_api:update")
         {
             return await VismaNetApiHelper.GetTokenFromVismaConnect(clientId,secret,tenant_id,scope);
         }
